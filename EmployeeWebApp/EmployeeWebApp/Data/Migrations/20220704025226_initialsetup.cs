275a9877a94856649e2462e91ceb139f7c19a878
emberspiritplayer03 <scplp03@gmail.com>
2022-07-04 11:09:04 +0800

    first commit

---
+++ EmployeeWebApp/EmployeeWebApp/Data/Migrations/20220704025226_initialsetup.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EmployeeWebApp.Data.Migrations
+{
+    public partial class initialsetup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employee",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EmpNo = table.Column<int>(type: "int", nullable: false),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Birthdate = table.Column<int>(type: "int", nullable: false),
+                    ContactNo = table.Column<int>(type: "int", nullable: false),
+                    EmailAddress = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employee", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employee");
+        }
+    }
+}
---
+++ EmployeeWebApp/EmployeeWebApp/Models/Employee.cs
@@ -0,0 +1,19 @@
+﻿namespace EmployeeWebApp.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public int EmpNo { get; set; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Birthdate { get; set; }
+        public int ContactNo { get; set; }
+        public string EmailAddress { get; set; }
+
+        public Employee()
+        {
+
+        }
+    }
+}
